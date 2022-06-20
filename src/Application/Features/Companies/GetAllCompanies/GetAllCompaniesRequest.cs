232dde224158a774403ee523906585ff44772078
Ahmed Magdy <ahmedmagdy4122@gmail.com>
2022-06-20 12:53:18 +0200

    minor update

--- src/Application/Features/Companies/GetAllCompanies/GetAllCompaniesRequest.cs
+++ src/Application/Features/Companies/GetAllCompanies/GetAllCompaniesRequest.cs
@@ -22,14 +22,16 @@ namespace ErpDashboard.Application.Features.Companies.GetAllCompanies
     internal class GetAllCompaniesRequestHandler : IRequestHandler<GetAllCompaniesRequest, IResult<List<GetAllCompaniesDto>>>
     {
         private readonly CustomIUnitOfWork<int> _unitOfWork;
-
-        public GetAllCompaniesRequestHandler(CustomIUnitOfWork<int> unitOfWork)
+        private readonly ICompanyRepository _repository;
+        public GetAllCompaniesRequestHandler(CustomIUnitOfWork<int> unitOfWork, ICompanyRepository repository)
         {
             _unitOfWork = unitOfWork;
+            _repository = repository;
         }
 
         public async Task<IResult<List<GetAllCompaniesDto>>> Handle(GetAllCompaniesRequest request, CancellationToken cancellationToken)
         {
+            
             var Companies = await _unitOfWork.Repository<TbCompany>().GetAllAsync();
             var MappedCompanies = Companies.Select(c => new GetAllCompaniesDto() { CompanyId = c.ComId, CompanyName = c.ComName, CompanySympol = c.CompanySymbol }).ToList();
             return await Result<List<GetAllCompaniesDto>>.SuccessAsync(MappedCompanies);
---
+++ src/Infrastructure/Repositories/CompanyRepository.cs
@@ -0,0 +1,28 @@
+﻿using ErpDashboard.Application.Interfaces.Repositories;
+using ErpDashboard.Application.Models;
+using ErpDashboard.Infrastructure.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErpDashboard.Infrastructure.Repositories
+{
+    public class CompanyRepository : CustomRepositoryAsync<TbCompany, int>, ICompanyRepository
+    {
+        private readonly ERBSYSTEMContext _dbContext;
+
+        public CompanyRepository(ERBSYSTEMContext dbContext): base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public bool IsCompanyNameExist()
+        {
+            
+            return false;
+        }
+    }
+}
